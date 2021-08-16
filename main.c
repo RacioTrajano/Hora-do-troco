099a0077cd8c6026ea5f2f7d73986ae5c1a57c4d
racio <85686582+RacioTrajano@users.noreply.github.com>
2021-08-16 17:26:32 +0000

    alterado

--- main.c
+++ main.c
@@ -3,9 +3,9 @@
 
 int repetido[4][20][20][20][20][20][20]={0};
 
-void troco (int money, int cem, int cinquenta, int vinte, int dez, int cinco, int dois, int real){
+int troco (int cont,int money, int cem, int cinquenta, int vinte, int dez, int cinco, int dois, int real){
 
-int cont =1;
+
 if (money == 0 && cont== 1){
   if (repetido[cem][cinquenta][vinte][dez][cinco][dois][real]==0){
   printf("Troco %d: ", cont);
@@ -39,29 +39,29 @@ repetido[cem][cinquenta][vinte][dez][cinco][dois][real]=1;
 
 else{
   if(money >= 100){
-    troco(money -100, cem +1, cinquenta, vinte, dez, cinco, dois, real);
+    cont =troco(cont,money -100, cem +1, cinquenta, vinte, dez, cinco, dois, real);
   }
   if(money >= 50){
-    troco(money -50, cem , cinquenta+1, vinte, dez, cinco, dois, real);
+    cont =troco(cont,money -50, cem , cinquenta+1, vinte, dez, cinco, dois, real);
   }
   if(money >= 20){
-    troco(money -20, cem , cinquenta, vinte+1, dez, cinco, dois, real);
+    cont =troco(cont,money -20, cem , cinquenta, vinte+1, dez, cinco, dois, real);
   }
   if(money >= 10){
-    troco(money -10, cem , cinquenta, vinte, dez+1, cinco, dois, real);
+    cont =troco(cont,money -10, cem , cinquenta, vinte, dez+1, cinco, dois, real);
   }
   if(money >= 5){
-    troco(money -5, cem , cinquenta, vinte, dez, cinco+1, dois, real);
+    cont =troco(cont,money -5, cem , cinquenta, vinte, dez, cinco+1, dois, real);
   }
   if(money >= 2){
-    troco(money -2, cem , cinquenta, vinte, dez, cinco, dois+1, real);
+    cont =troco(cont,money -2, cem , cinquenta, vinte, dez, cinco, dois+1, real);
   }
   if(money >= 1){
-    troco(money -1, cem , cinquenta, vinte, dez, cinco, dois, real+1);
+    cont =troco(cont,money -1, cem , cinquenta, vinte, dez, cinco, dois, real+1);
   }
 }
 
-
+return cont;
 
 }
 
@@ -69,10 +69,11 @@ else{
 
 int main () {
 
+int cont=1;
 int dinheiro;
 scanf ("%d", &dinheiro);
 
-troco(dinheiro,0,0,0,0,0,0,0);
+troco(cont,dinheiro,0,0,0,0,0,0,0);
 
 
   return 0;
